409173244c95db6d15d189ddb91b4380802d7412
iSlesa <artycrab_slesh@yahoo.com>
2016-06-17 13:44:00 +0545

    Add transparent overlay lockscreen

--- i3/i3lock/i3lock.sh
+++ i3/i3lock/i3lock.sh
@@ -2,9 +2,11 @@
 
 image=~/.config/i3/i3lock/i3lock.png
 lock=~/.config/i3/i3lock/lock.png
+screen=~/.config/i3/i3lock/lockscreen.png
+#this=~/.config/i3/i3lock/yolo.png
 rm $image -f
 scrot $image
-mogrify -colorspace Gray $lock
+#mogrify -colorspace Gray $lock
+composite -gravity center -blend 50 $screen $image $image
 mogrify -blur 5x9+90 -gravity center -draw "image over 0,0 0,0 '$lock'" $image
-#composite -gravity center $lockicon  $image $image
 i3lock -u -b -p default -i $image
