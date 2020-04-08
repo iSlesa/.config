2cf963e8c6c2f95504fea083a6457c8fa79a979f
Slesa Adhikari <slesaad@gmail.com>
2020-04-08 16:31:10 -0500

    Update for MatebookXPRO April, 2020

--- i3/i3lock/i3lock.sh
+++ i3/i3lock/i3lock.sh
@@ -3,7 +3,6 @@
 image=~/.config/i3/i3lock/i3lock.png
 lock=~/.config/i3/i3lock/lock.png
 screen=~/.config/i3/i3lock/lockscreen.png
-#this=~/.config/i3/i3lock/yolo.png
 rm $image -f
 scrot $image
 #mogrify -colorspace Gray $image
